4d6d41e21726e0f95e1e6a2378fd30fcff3fff74
Alenmnoian4 <alenmnoian4@gmail.com>
2022-11-09 22:39:04 -0500

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -6,5 +6,12 @@ app.get('/greeting/', (request, response) => {
 app.get('/greeting/:name', (request, response) => {
 	response.send(`Hello, ${request.params.name}`);
 });
-
-app.listen(3002);
+app.get('/tip/:total/:tipPercentage', (request, response) => {
+	response.send(
+		`${tipTotal(request.params.total, request.params.tipPercentage)}`
+	);
+});
+function tipTotal(total, TipPercentage) {
+	return total * (TipPercentage / 100);
+}
+app.listen(3002)
