21ea1e1cedd8dcff70c3f7ee1b48b57540b9b10e
Alenmnoian4 <alenmnoian4@gmail.com>
2022-11-09 22:59:56 -0500

    Greeting express application created

--- server.js
+++ server.js
@@ -6,12 +6,6 @@ app.get('/greeting/', (request, response) => {
 app.get('/greeting/:name', (request, response) => {
 	response.send(`Hello, ${request.params.name}`);
 });
-app.get('/tip/:total/:tipPercentage', (request, response) => {
-	response.send(
-		`${tipTotal(request.params.total, request.params.tipPercentage)}`
-	);
-});
-function tipTotal(total, TipPercentage) {
-	return total * (TipPercentage / 100);
-}
-app.listen(3002)
+
+app.listen(3002);
+
